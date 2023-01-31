9e88e800b8684609a7ccb39913efe9005d465818
vincentprr <furyax13@gmail.com>
2023-01-31 21:46:30 +0100

    feat: Replacing static property in `UserManager` to a const

--- Managers/UserManager.php
+++ Managers/UserManager.php
@@ -7,12 +7,12 @@ use Enums\RegistrationResult;
 use Models\User;
 
 class UserManager{
-    private static string $mailRegex = "^[a-z_\\-0-9]+@[a-z\\-0-9]+\\.[a-z]+$";
+    const mailRegex = "^[a-z_\\-0-9]+@[a-z\\-0-9]+\\.[a-z]+$";
 
     public static function register(DatabaseAccessor $db, string $email, string $password, string $passwordCheck, string $firstName, string $lastName) : RegistrationResult{
         $result = RegistrationResult::Success;
 
-        if (preg_match(self::$mailRegex, $email)){
+        if (preg_match(self::mailRegex, $email)){
             
         }
         else

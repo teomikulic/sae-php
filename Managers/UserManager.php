135527e8506fbbaf9c0b49d88be96c049e135329
vincentprr <furyax13@gmail.com>
2023-01-31 21:27:28 +0100

    chore(Users): Retrieving getters on `UserManager`.

--- Managers/UserManager.php
+++ Managers/UserManager.php
@@ -2,28 +2,11 @@
 
 namespace Managers;
 
+use BubbleORM\DatabaseAccessor;
 use Models\User;
 
 class UserManager{
-    public static function getUser(callable $f) : User{
-        global $db;
-
-        return $db->createQuery(User::class)
-            ->where($f)
-            ->firstOrDefault();
-    }
-
-    public static function getUsers(callable $f) : array{
-        global $db;
-
-        return $db->createQuery(User::class)
-            ->where($f)
-            ->all();
-    }
-
-    public static function register(string $email, string $password, string $firstName, string $lastName) : void{
-        global $db;
-
+    public static function register(DatabaseAccessor $db, string $email, string $password, string $firstName, string $lastName) : void{
         
     }
 }

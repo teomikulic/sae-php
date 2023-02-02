4ad78a40deda9173dead03829fca61570db877d0
vincentprr <furyax13@gmail.com>
2023-02-02 14:33:18 +0100

    fix(Quiz): Changing quiz management about questions.

--- Managers/QuizManager.php
+++ Managers/QuizManager.php
@@ -6,15 +6,19 @@ use BubbleORM\DatabaseAccessor;
 use Models\Quiz;
 
 class QuizManager{
+    private static function assignQuestions(DatabaseAccessor $db, Quiz $quiz) : void{
+        $quiz->setQuestions($db->createQuery(Question::class)
+            ->where(fn($question) => $question->quizId == $quiz->id)
+            ->all());
+    }
+
     public static function getQuiz(DatabaseAccessor $db, int $quizId) : ?Quiz{
         $quiz = $db->createQuery(Quiz::class)
             ->where(fn($quiz) => $quiz->id == $quizId)
             ->firstOrDefault();
         
         if($quiz) // If quiz isn't null then we load the questions. Because I havn't finish my ORM :'(
-            $quiz->setQuestions($db->createQuery(Question::class)
-                ->where(fn($question) => $question->quizId == $quizId)
-                ->all());
+            self::assignQuestions($db, $quiz);
 
         return $quiz;
     }

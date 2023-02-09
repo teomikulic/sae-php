dc214ff3f6d7e1c46ae3378b485ebf464d36400c
vincentprr <furyax13@gmail.com>
2023-02-09 16:09:28 +0100

    feat(Connection)!: Adding front of the connection system.

--- Imports/js/main.js
+++ Imports/js/main.js
@@ -1,19 +1,8 @@
 const POPUP = document.getElementById("popup");
-const POPUP_CONTENT = document.getElementById("popup_content");
 
-function popup(content, tagName, width = null){
-    if(POPUP != null && POPUP_CONTENT != null){
+function popup(){
+    if(POPUP != null){
         POPUP.style.display = "flex";
-
-        if(POPUP.getAttribute("type") != tagName){
-            if(width != null)
-                POPUP_CONTENT.style.width = width;
-
-            POPUP.setAttribute("type", tagName);
-
-            POPUP_CONTENT.innerHTML = content;
-        }
-
         document.body.style.overflow = "hidden";
     }
 }
@@ -23,8 +12,4 @@ function closePopup(){
         POPUP.style.display = "none";
         document.body.style.overflow = "";
     }
-}
-
-function loginPopup(){
-    popup(LOGIN_FORM, "login", "30%");
 }
